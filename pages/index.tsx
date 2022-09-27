eb641763043c167fc4269e29fd9818eaecd6c814
Daniel Don <dondaniel028@gmail.com>
2022-09-27 10:28:38 +0100

    Added Acid UI renderer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Render } from "acid-ui";
 import type { NextPage } from "next";
 import Image from "next/image";
 import { Col, Container, Row } from "react-bootstrap";
@@ -11,56 +12,68 @@ const Home: NextPage = () => {
 			<div className="flex flex-col h-screen w-screen justify-center align-center text-center border border-1">
 				<Container>
 					<Row className="justify-content-center mb-5">
-						<Col md={5} style={{ height: "200px" }}>
-							<Lottie
-								options={{
-									loop: true,
-									autoplay: true,
-									animationData: construction,
-								}}
-							/>
+						<Col
+							md={5}
+							style={{
+								height: "200px",
+								position: "relative",
+							}}
+						>
+							<Render type="block">
+								<Lottie
+									options={{
+										loop: true,
+										autoplay: true,
+										animationData: construction,
+									}}
+									height={200}
+								/>
+							</Render>
 						</Col>
 					</Row>
 					<Row className="justify-content-center">
 						<Col md={5}>
-							<h1 className="text-5xl">Hi, Daniel Here.</h1>
-							<p>
-								This portfolio site is currently undergoing a
-								redesign, <br />
-								so please bear with me for a week or two. 🤲
-							</p>
-							<br />
-							<br />
-							<p className="flex flex-col justify-center align-center text-center">
-								Meanwhile, you can check me out on LinkedIn
-								<a
-									className="mt-3"
-									target="_blank"
-									rel="noreferrer"
-									href="https://www.linkedin.com/in/don-daniel-748414190"
-								>
-									<Image
-										src={"/assets/LinkedIn_logo.png"}
-										width={30}
-										height={30}
-									/>
-								</a>
-							</p>
-							<p className="flex flex-col justify-center align-center text-center mt-3">
-								Or Github
-								<a
-									className="mt-1"
-									target="_blank"
-									rel="noreferrer"
-									href="https://github.com/dondxniel"
-								>
-									<Image
-										src={"/assets/Github-logo.png"}
-										width={30}
-										height={30}
-									/>
-								</a>
-							</p>
+							<Render type="inline">
+								<h1 className="text-5xl">Hi, Daniel Here.</h1>
+								<p>
+									This portfolio site is currently undergoing
+									a redesign, <br />
+									so please bear with me for a week or two. 🤲
+								</p>
+								<br />
+								<br />
+								<p className="flex flex-col justify-center align-center text-center">
+									Meanwhile, you can check me out on LinkedIn
+									<a
+										className="mt-3"
+										target="_blank"
+										rel="noreferrer"
+										href="https://www.linkedin.com/in/don-daniel-748414190"
+									>
+										<Image
+											src={"/assets/LinkedIn_logo.png"}
+											width={30}
+											height={30}
+										/>
+									</a>
+								</p>
+
+								<p className="flex flex-col justify-center align-center text-center mt-3">
+									Or Github
+									<a
+										className="mt-1"
+										target="_blank"
+										rel="noreferrer"
+										href="https://github.com/dondxniel"
+									>
+										<Image
+											src={"/assets/Github-logo.png"}
+											width={30}
+											height={30}
+										/>
+									</a>
+								</p>
+							</Render>
 						</Col>
 					</Row>
 				</Container>
